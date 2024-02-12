0e5d6efa0070421a60835cf5bc0f3563911c2cd0
RabieP <m.rabiepatel@gmail.com>
2024-02-12 19:32:25 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,43 @@
+ <!-- Masthead-->
+ <html>
+ <head>
+    <style>.page-section {
+        background: rgb(0, 0, 0);/* Fallback */
+        background: rgba(0, 0, 0, 1.0);
+      }
+  </style>
+</head>
+</html>
+
+        
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+ <html>
+ <head>
+    <style>.page-section {
+        background: rgb(0, 0, 0);/* Fallback */
+        background: rgba(0, 0, 0, 0.6);
+      }
+  </style>
+</head>
+</html>
+
+
+    <section class="page-section">
+    <div class="container">
+   
+
+    <?php echo html_entity_decode($_SESSION['system']['about_content']) ?>        
+            
+        </div>
+        </section>
